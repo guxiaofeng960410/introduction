5a4f5aa0783fac074901cda14939bfccdef17b47
guxiaofeng <gxfgxfgxf960410@163.com>
2019-04-03 15:19:47 +0800

    2019/4/3

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports ={
     mode: 'development',
@@ -6,13 +8,20 @@ module.exports ={
     entry: __dirname +'/public/index.js',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname + '/dist')
     },
-    devtool: 'inline-source-map',
+    devtool: 'source-map',
     devServer:{
         contentBase: __dirname + '/dist',
         port: 3000,
     },
+    
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: __dirname + '/view/index.html'
+        })
+    ],
     module:{
         rules:[
             {
